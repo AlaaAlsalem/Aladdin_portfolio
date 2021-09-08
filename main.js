d8b8bb9e679fd76747b044beb56076c9fdd457af
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-09-08 20:38:08 +0300

    style the mobile view to fit the hall screen

--- main.js
+++ main.js
@@ -1,54 +1,3 @@
-/* const mobileMenuDiv = document.querySelector('#mob_link');
-mobileMenuDiv.className = 'menu-hidden';
-let anchorTag = document.createElement('a');
-anchorTag.setAttribute('href', '/');
-anchorTag.style.display = 'flex';
-anchorTag.style.justifyContent = 'flex-end';
-anchorTag.style.marginBottom = '10%';
-anchorTag.style.marginRight = '10%';
-const closingIcon = document.createElement('IMG');
-closingIcon.setAttribute('src', 'Images/Icon.png');
-closingIcon.setAttribute('alt', 'icon image');
-closingIcon.id = 'close-button';
-anchorTag.appendChild(closingIcon);
-mobileMenuDiv.appendChild(anchorTag);
-anchorTag = document.createElement('a');
-anchorTag.textContent = 'Portfolio';
-anchorTag.setAttribute('href', '#actual-work-section');
-anchorTag.className = 'links';
-mobileMenuDiv.appendChild(anchorTag);
-anchorTag = document.createElement('a');
-anchorTag.textContent = 'About';
-anchorTag.setAttribute('href', '#menu-about');
-anchorTag.className = 'links';
-mobileMenuDiv.appendChild(anchorTag);
-anchorTag = document.createElement('a');
-anchorTag.textContent = 'Contact';
-anchorTag.setAttribute('href', '#menu-contact');
-anchorTag.className = 'links';
-mobileMenuDiv.appendChild(anchorTag);
-const menuButton = document.querySelector('#nav_btn');
-
-function display() {
-  const targetedDiv = document.querySelector('.menu-hidden');
-  targetedDiv.classList.add('show');
-  menuButton.style.display = 'none';
-}
-
-menuButton.addEventListener('click', display);
-const closeButton = document.querySelector('#close-button');
-function closePop() {
-  const targetedDiv2 = document.querySelector('.menu-hidden');
-  targetedDiv2.classList.remove('show');
-  menuButton.style.display = 'block';
-}
-
-closeButton.addEventListener('click', closePop);
-const anchorTagLinks = document.querySelectorAll('.links');
-anchorTagLinks.forEach((link) => {
-  link.addEventListener('click', closePop);
-}); */
-
 const mobileMenuDiv = document.querySelector('#mob_link');
 mobileMenuDiv.className = 'menu-hidden';
 let anchorTag = document.createElement('a');
@@ -82,21 +31,21 @@ anchorTag.className = 'menu-links';
 mobileMenuDiv.appendChild(anchorTag);
 const menuButton = document.querySelector('#nav-btn');
 
-function display() {
+const display = () => {
   const targetedDiv = document.querySelector('.menu-hidden');
   targetedDiv.classList.add('visible');
   mobileMenuDiv.style.visibility = 'visible';
   menuButton.style.display = 'none';
-}
+};
 
 const closeMenu = document.querySelector('.closeMenu');
 menuButton.addEventListener('click', display);
-function closePop() {
+const closePop = () => {
   const targetedDiv2 = document.querySelector('.menu-hidden');
   targetedDiv2.style.display = 'none';
   menuButton.style.display = 'block';
   closeMenu.style.display = 'none';
-}
+};
 const anchorTagLinks = document.querySelectorAll('.menu-links');
 anchorTagLinks.forEach((link) => {
   link.addEventListener('click', closePop);
