5af97c47d5f66e08620915d13da99ab632fda3d0
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-09-09 15:18:34 +0300

    add function to form.js to handl the event listener on submit and check if for errors

--- form.js
+++ form.js
@@ -1,5 +1,12 @@
 const emailAddress = document.getElementById('myEmail');
 const templateForm = document.getElementById('form_id');
 const validatorMessage = document.getElementById('validator-message');
-
+templateForm.addEventListener('submit', (e) => {
+    if (emailAddress.value !== emailAddress.value.toLowerCase()) {
+      e.preventDefault();
+      validatorMessage.textContent = 'Kindly input a valid email address!';
+    } else {
+      validatorMessage.textContent = '';
+    }
+  });
   
