fbd1edad785e739c1aafe1cea6bb4e1effbad65c
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-09-09 15:13:45 +0300

    creadte a validation form to handel user input errors

---
+++ form.js
@@ -0,0 +1,5 @@
+const emailAddress = document.getElementById('myEmail');
+const templateForm = document.getElementById('form_id');
+const validatorMessage = document.getElementById('validator-message');
+
+  
